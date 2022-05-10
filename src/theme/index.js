33420cb3d5fe8a562c97792bcbecb76f2b06dda8
Abhinav <abhinavbhateja@gmail.com>
2022-05-10 17:33:42 +0530

    Initialized Chakra UI with color palette

---
+++ src/theme/index.js
@@ -0,0 +1,12 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+    colors: {
+        dark: "#222831",
+        gray: "#393E46",
+        accent: "#00ADB5",
+        light: "#EEEEEE"
+    }
+})
+
+export default theme;
