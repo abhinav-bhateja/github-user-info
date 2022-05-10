e10df54e086fdc5d4b34c403574bce71dd1e162f
Abhinav <abhinavbhateja@gmail.com>
2022-05-11 01:03:36 +0530

    Set up Search Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+import {Box, Container} from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
+import Search from "./components/Search";
 
 function App() {
-  return (
-    <>
-    </>
-  );
+    return (
+        <Box>
+            <Navbar/>
+            <Container mt={{base: '8', lg: '16'}}>
+                <Search/>
+            </Container>
+        </Box>
+    );
 }
 
 export default App;
